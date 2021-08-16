a69c65c5cbc06cd0765a281970508ef5f40d7c90
nathanmaru <nathanmaru7@gmail.com>
2021-08-16 11:19:11 +0800

    added quill instance

--- meegu-frontend/src/components/project/projectmanager.jsx
+++ meegu-frontend/src/components/project/projectmanager.jsx
@@ -1,5 +1,20 @@
+import { useQuill } from 'react-quilljs';
+// or const { useQuill } = require('react-quilljs');
+
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+// or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
+
 const ProjectManager = () => {
-	return null;
+	const { quill, quillRef } = useQuill();
+
+	console.log(quill); // undefined > Quill Object
+	console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
+
+	return (
+		<div style={{ width: 900, height: 300 }}>
+			<div ref={quillRef} />
+		</div>
+	);
 };
 
 export default ProjectManager;
