3f32a060a4c8e1d66a4579f6ac8b997f28410d1b
nathanmaru <nathanmaru7@gmail.com>
2021-09-09 13:54:19 +0800

    Project modulet

--- backend-api/projects/models.py
+++ backend-api/projects/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from accounts.models import UserAccount
 
 # Create your models here.
 
+
+class Project(models.Model):
+    STATUS_OPTIONS = [
+        ("PUBLISHED", "PUBLISHED"),
+        ("ONGOING", "ONGOING"),
+        ("REVISION", "REVISION"),
+        ("SUBMITTED", "SUBMITTED"),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=50, choices=STATUS_OPTIONS, default="ONGOING")
+    members = models.ManyToManyField(
+        "UserAccount",
+    )
